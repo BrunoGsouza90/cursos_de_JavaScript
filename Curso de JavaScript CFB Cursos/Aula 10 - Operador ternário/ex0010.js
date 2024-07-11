4330a74e282534b1df05a3a43c066b575157df46
Bruno Gonçalves de Souza <bruno.gsouza199@gmail.com>
2024-07-11 00:09:57 -0300

    Cursos de JavaScript

---
+++ Curso de JavaScript CFB Cursos/Aula 10 - Operador ternário/ex0010.js
@@ -0,0 +1,18 @@
+var num = 10
+var resultado = num % 2
+if(resultado==0){
+    console.log("Número Par!")
+}else{
+    console.log("Número Impar!")
+}
+
+var num = 10
+resultado = (!(num % 2) ? "Par" : "Ímpar")
+console.log(resultado)
+
+var num1 = 10
+var num2 = 5
+console.log((num1 > num2) ? "Num1 é maior!" : "Num2 é maior!")
+
+var statuss = 'A'
+console.log(statuss == 'A' ? "Ativo!" : "Inativo!")
---
+++ Curso de JavaScript CFB Cursos/Aula 9 - Pré incremento e Pós incremento/aula009.js
@@ -0,0 +1,35 @@
+var n = 10
+
+n++
+
+console.log(n)
+
+var n = 10
+
+console.log(n++)
+
+var n = 10
+
+console.log(++n)
+
+var n = 10
+
+console.log(n++)
+
+console.log(n)
+
+n += 10
+
+console.log (n)
+
+var n = 10
+
+console.log(--n)
+
+var n1 = 10
+var n2 = 10
+
+console.log(n1 + n2)
+
+console.log(String(n1) + String(n2))
+console.log(`${n1}${n2}`)
