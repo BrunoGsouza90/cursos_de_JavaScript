69659bfe903f784bb6c3fd805ddd2d8e6cf86d16
Bruno Gonçalves de Souza <bruno.gsouza199@gmail.com>
2024-07-25 20:53:04 -0300

    CFB Cursos - Aula 30

--- Curso de JavaScript CFB Cursos/Aula 30 - getElementById/ex0030.js
+++ Curso de JavaScript CFB Cursos/Aula 30 - getElementById/ex0030.js
@@ -1,16 +1,3 @@
-function somar_Dois(num){
-    return num + 2
-}
-
-lista = [1,2,3,4,5,6].map(somar_Dois)
-console.log(lista)
-
-var multiplicar_Dois = (num) => {
-    return num * 2
-}
-
-console.log([2,4,6,8,10].map(multiplicar_Dois))
-
 var div1 = window.document.getElementById('c1')
 var div2 = window.document.getElementById('c2')
 var div3 = window.document.getElementById('c3')
@@ -19,6 +6,11 @@ var div5 = window.document.getElementById('c5')
 var div6 = window.document.getElementById('c6')
 
 var lista = [div1, div2, div3, div4, div5, div6]
+console.log(div1)
+console.log(div1.id)
+console.log(div1.innerHTML)
+div1.innerHTML = 'CFB Cursos'
+console.log(lista)
 
 for(div in lista){
     lista[div].innerHTML = 'CFB Cursos'
