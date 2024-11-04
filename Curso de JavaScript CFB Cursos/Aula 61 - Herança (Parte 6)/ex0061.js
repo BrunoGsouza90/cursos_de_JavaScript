c6d8b19e44b04342c2d2289119dfae00e6ccb1f8
Bruno Gonçalves de Souza <bruno.gsouza199@gmail.com>
2024-11-04 19:48:38 -0300

    Aula 61

--- Curso de JavaScript CFB Cursos/Aula 61 - Herança (Parte 6)/ex0061.js
+++ Curso de JavaScript CFB Cursos/Aula 61 - Herança (Parte 6)/ex0061.js
@@ -1,5 +1,65 @@
 let carros = document.querySelector('#carros')
 let paragrafo = document.createElement('p')
-caixa.appendChild(paragrafo)
+carros.appendChild(paragrafo)
 paragrafo.setAttribute('style', 'text-align:center;')
+let carro = 0
 
+class Carros{
+    constructor(pnome, portas, pcor, pvelocidade){
+        this.nome = pnome
+        this.portas = portas
+        this.ligado = 'Não'
+        this.velocidade = pvelocidade
+        this.cor = pcor
+    }
+
+}
+
+class Militar extends Carros{
+    constructor(pblindagem, pmunicao, nome, portas, cor, velocidade){
+        super(nome, portas, cor = "Vermelho", velocidade = '50')
+        this.blindagem = pblindagem
+        this.municao = pmunicao
+    }
+
+    atirar = ()=>{
+        if(this.municao > 0){
+            this.municao--
+        } else {
+            this.municao = 'Sem munição!'
+        }
+    }
+
+    info(){
+        carro += 1
+        paragrafo.innerHTML += `<span style="color: green;"><strong>${carro}º Carro:</strong></span><br><br>`
+        paragrafo.innerHTML += `Nome: ${this.nome}<br>`
+        paragrafo.innerHTML += `Número de portas: ${this.portas}<br>`
+        paragrafo.innerHTML += `Ligado: ${this.ligado}<br>`
+        paragrafo.innerHTML += `Velocidade: ${this.velocidade} km/h<br>`
+        paragrafo.innerHTML += `Cor: ${this.cor}<br>`
+        paragrafo.innerHTML += `Munição: ${this.municao}<br>`
+        paragrafo.innerHTML += `Blidagem: ${this.blindagem}<br><br>`
+    }
+
+    ligar = ()=>{
+        this.ligado = 'Sim'
+    }
+
+    desligar = ()=>{
+        this.ligado = 'Não'
+    }
+
+    setCor = (pcor)=>{
+        this.cor = pcor
+    }
+
+}
+
+let carro1 = new Militar(100, 1, "Tanque")
+
+carro1.atirar()
+carro1.atirar()
+carro1.ligar()
+carro1.desligar()
+carro1.info()
