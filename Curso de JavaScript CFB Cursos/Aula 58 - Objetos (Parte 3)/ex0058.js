cb42c6a68e37528b33cbc625325d36e684b77780
Bruno Gonçalves de Souza <bruno.gsouza199@gmail.com>
2024-11-04 16:18:09 -0300

    Aula 58 - CFB Cursos

---
+++ Curso de JavaScript CFB Cursos/Aula 58 - Objetos (Parte 3)/ex0058.js
@@ -0,0 +1,37 @@
+let caixa_nome = document.querySelector('#caixa_nome')
+let caixa_idade = document.querySelector('#caixa_idade')
+let adicionar = document.querySelector('#adicionar')
+let caixa_resposta = document.querySelector('#caixa_resposta')
+let pessoa = 0
+
+class Pessoa{
+    constructor(pnome, pidade){
+        this.nome = pnome
+        this.idade = pidade
+    }
+
+    info(){
+        pessoa += 1
+        caixa_resposta.innerHTML += `<span style="color: green;"><strong>${pessoa}º Pessoa:</strong></span><br><br>`
+        caixa_resposta.innerHTML += `Nome: ${this.nome}<br>`
+        caixa_resposta.innerHTML += `Idade: ${this.idade}<br><br>`
+    }
+}
+
+adicionar.addEventListener('click', (event)=>{
+    event.preventDefault();
+
+    let nome = caixa_nome.value
+    let idade = caixa_idade.value
+
+    if(nome == ''){
+        nome = 'Nome não mencionado!'
+    }
+
+    if(idade == ''){
+        idade = 'Idade não mencionada!'
+    }
+
+    let p1 = new Pessoa(`${nome}`, `${idade}`)
+    p1.info()
+})
